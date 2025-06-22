3201a2726d2560a5c31cc2a9d9cf1ea01d4f4303
ichsanup <ichsanup@gmail.com>
2025-06-22 20:53:26 +0700

    Test Fix again

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from App import is_evenNumber, is_palindrome
+from app import is_evenNumber, is_palindrome
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
