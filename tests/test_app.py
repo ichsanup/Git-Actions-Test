f37fc612cbdde84a70e17be8e93b7af8fe428b34
ichsanup <ichsanup@gmail.com>
2025-06-22 21:15:13 +0700

    refactor folder

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,10 @@ from app import is_evenNumber, is_palindrome
 
 
 def test_is_evenNumber():
-    assert is_evenNumber(4) == True
-    assert is_evenNumber(5) == False
+    assert is_evenNumber(4) is True
+    assert is_evenNumber(5) is False
 
 
 def test_is_palindrome():
-    assert is_palindrome("katak") == True
-    assert is_palindrome("tunggu") == False
+    assert is_palindrome("katak") is True
+    assert is_palindrome("tunggu") is False
